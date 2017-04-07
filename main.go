e63c28e1cc11c2169f87e779e9b64954b89bd188
Abhijit Hoskeri <abhijithoskeri@gmail.com>
2017-04-07 14:22:36 -0700

    more progress

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import "path/filepath"
 import "github.com/omeid/go-livereload"
 import "gopkg.in/fsnotify.v1"
 
+type BuildAction interface {
+	FileChanged(path string) error
+}
+
 type FileSet struct {
 	pattern string
 	matched []string
@@ -24,6 +28,7 @@ func (f *FileSet) Rescan() (err error) {
 	}
 
 	for _, e := range f.matched {
+		log.Debugf("watching file %s", e)
 		err = f.watcher.Add(e)
 		if err != nil {
 			f.watcher.Close()
@@ -35,25 +40,25 @@ func (f *FileSet) Rescan() (err error) {
 
 func (r *LiveRebuild) Run() error {
 	r.server = livereload.New("liverebuild")
-	r.watchFileSet.Rescan()
-	r.buildFileSet.Rescan()
+	log.Errorln(r.watchFileSet.Rescan())
+	log.Errorln(r.buildFileSet.Rescan())
 
 	for {
 		select {
 		case event := <-r.watchFileSet.watcher.Events:
 			if event.Op&(fsnotify.Rename|fsnotify.Create|fsnotify.Write) > 0 {
-				log.Debug("running watchAction %s\n", r.buildAction)
+				log.Debug("running watchAction %s", r.buildAction)
 			}
 		case event := <-r.buildFileSet.watcher.Events:
 			if event.Op&(fsnotify.Rename|fsnotify.Create|fsnotify.Write) > 0 {
-				log.Debug("running buildAction %s\n", r.buildAction)
+				log.Debug("running buildAction %s", r.buildAction)
 			}
 
 		case e := <-r.buildFileSet.watcher.Errors:
-			log.Debugf("caught error %s\n", e)
+			log.Debugf("caught error %s", e)
 
 		case e := <-r.watchFileSet.watcher.Errors:
-			log.Debugf("caught error %s\n", e)
+			log.Debugf("caught error %s", e)
 		}
 	}
 	return nil
@@ -74,12 +79,14 @@ func main() {
 	flag.StringVar(&service.buildAction, "onbuild", "", "shell command on build file change")
 	flag.StringVar(&service.buildFileSet.pattern, "buildfiles", "", "set of files to rebuild")
 	flag.StringVar(&service.watchFileSet.pattern, "watchfiles", "", "set of files to livereload")
-	flag.StringVar(&service.watchServeRoot, "root", "", "set of files to livereload")
-	flag.StringVar(&service.watchServeFallback, "fallback", "index.html", "set of files to livereload")
+	flag.StringVar(&service.watchServeRoot, "root", "", "static server document root")
+	flag.StringVar(&service.watchServeFallback, "fallback", "index.html", "path to render on fallback")
+
+	log.SetLevel(log.DebugLevel)
 
 	flag.Parse()
-	log.Println("starting liverebuild")
 
+	log.Println("starting liverebuild")
 	service.Run()
 	log.Println("liverebuild is running")
 }
