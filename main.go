6d856b72817647d362e134145aa3a693c77cfdb6
Abhijit Hoskeri <abhijithoskeri@gmail.com>
2018-08-20 12:25:47 -0700

    parse config from toml

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"github.com/BurntSushi/toml"
 	"github.com/fsnotify/fsnotify"
 	log "github.com/hoskeri/liverebuild/llog"
 	"github.com/hoskeri/liverebuild/updater"
@@ -20,7 +21,48 @@ func (r *LiveRebuild) Run() (err error) {
 	return
 }
 
+type staticAction struct {
+	Address  string
+	Paths    []string
+	Fallback string
+}
+
+type lrAction struct {
+	Address string
+}
+
+type buildAction struct {
+	Paths []string
+	Cmd   string
+}
+
+type daemonAction struct {
+	Paths []string
+	Cmd   string
+}
+
+type config struct {
+	Verbose bool
+	Static  staticAction
+	Lr      lrAction
+	Build   buildAction
+	Daemon  daemonAction
+}
+
+var Config = config{}
+
+func parseConfig(cf string) error {
+	_, err := toml.DecodeFile(cf, &Config)
+	return err
+}
+
 func main() {
+	if err := parseConfig(".liverebuildrc"); err != nil {
+		log.Fatalf("failed to parse .liverebuildrc: %+v", err)
+	} else {
+		log.Debugf("parsed config: %+v", Config)
+	}
+
 	service := new(LiveRebuild)
 
 	var _ = updater.Nothing{}
