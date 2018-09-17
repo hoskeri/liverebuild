78d5471c73c4ed2766b19b62ae86202cf1ad787b
Abhijit Hoskeri <abhijithoskeri@gmail.com>
2018-09-16 22:07:33 -0700

    log cleanup

--- llog/log.go
+++ llog/log.go
@@ -4,21 +4,20 @@ import stdlog "log"
 
 type Level string
 
-const lDebug = Level("[debug] ")
-const lFatal = Level("[fatal] ")
-const lInfo = Level("[info] ")
+const lDbug = Level("[debug] ")
+const lInfo = Level("[ info] ")
 
 var Verbose = false
 
 func logLn(lvl Level, v ...interface{}) {
-	if lvl == lDebug && !Verbose {
+	if lvl == lDbug && !Verbose {
 		return
 	}
 	stdlog.Print(append([]interface{}{string(lvl)}, v...)...)
 }
 
 func logF(lvl Level, fmt string, v ...interface{}) {
-	if lvl == lDebug && !Verbose {
+	if lvl == lDbug && !Verbose {
 		return
 	}
 	stdlog.Printf(string(lvl)+" "+fmt, v...)
@@ -27,8 +26,8 @@ func logF(lvl Level, fmt string, v ...interface{}) {
 func Info(v ...interface{})            { logLn(lInfo, v...) }
 func Infof(f string, v ...interface{}) { logF(lInfo, f, v...) }
 
-func Debug(v ...interface{})            { logLn(lDebug, v...) }
-func Debugf(f string, v ...interface{}) { logF(lDebug, f, v...) }
+func Debug(v ...interface{})            { logLn(lDbug, v...) }
+func Debugf(f string, v ...interface{}) { logF(lDbug, f, v...) }
 
 func Fatal(v ...interface{})            { stdlog.Fatal(v...) }
 func Fatalf(f string, v ...interface{}) { stdlog.Fatalf(f, v...) }
