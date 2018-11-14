e4fc7bc8b41df47cb31fd9a47fee9450acc61ae2
Abhijit Hoskeri <abhijithoskeri@gmail.com>
2018-11-14 14:28:26 -0800

    hide dotfiles on http server

--- updater/static.go
+++ updater/static.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -19,14 +20,23 @@ func (s *StaticServer) Update(ts time.Duration, path ...string) {
 	return
 }
 
+func allowPath(pth string) bool {
+	for _, s := range strings.Split(pth, "/") {
+		if strings.HasPrefix(s, ".") {
+			return false
+		}
+	}
+	return true
+}
+
 func NewStaticServer(address, dir, fallback string) (*StaticServer, error) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		var lp = filepath.Join(dir, filepath.Clean(r.URL.EscapedPath()))
-		// FIXME: prevent escape from current dir,
-		// FIXME: don't show hidden files,
-		// FIXME: don't show files not owned.
-		// FIXME: don't follow symlinks out of root
+		if !allowPath(lp) {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
 		if _, err := os.Stat(lp); err == nil {
 			http.ServeFile(w, r, lp)
 		} else {
